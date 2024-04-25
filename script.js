469b31a2b8f80618981c4b217624270b2d18a629
ricmenezesdev <ricmenezesdev@gmail.com>
2024-04-25 19:36:19 -0400

    index

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ function displayWord(word) {
     var outputBox = document.getElementById("outputBox");
     outputBox.textContent = word;
 }
-
 function fetchWord() {
     fetch('logic.py')
     .then(response => response.text())
     .then(word => {
-        // Call the displayWord function to display the word
         displayWord(word);
-        // Print the word to the console
         console.log(word);
     })
     .catch(error => console.error('Error:', error));
 }
-
-// Call the fetchWord function when the window loads
 window.onload = fetchWord;
